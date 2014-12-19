63f765c8474326635279b2374cb3a98b07ceb97b
rizarifansyah <rizarifansyah1@gmail.com>
2014-12-19 16:53:18 +0700

    animate

--- assets/js/tekno.js
+++ assets/js/tekno.js
@@ -1,4 +1,14 @@
 /* button Search */
+	$(window).ready(function() {
+		$('.eff-animate').addClass("hide-el").viewportChecker({
+			classToAdd: 'visible animated bounceInUp', // Class to add to the elements when they are visible
+			offset: 100    
+		   });   
+	});
+	
+	
 	$(function(){ 
 		$('#search-2').popover()
 	});
+	
+	
