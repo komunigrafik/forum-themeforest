737553a81f9cb7b212c0d8cb1b6899d4f34be51f
rizarifansyah <rizarifansyah1@gmail.com>
2015-01-05 10:11:12 +0700

    resposive-index

--- assets/js/scrolling-nav.js
+++ assets/js/scrolling-nav.js
@@ -2,11 +2,11 @@
 $(window).scroll(function() {
     if ($(".nav-top").offset().top > 50) {
         $(".nav-top").addClass("fixed-top");
-		$(".nav-top").addClass("fadeInUp");
+		$(".nav-top").addClass("fadeInDown");
 		$(".nav-top").addClass("animated");
     } else {
         $(".nav-top").removeClass("fixed-top");
-		$(".nav-top").removeClass("fadeInUp");
+		$(".nav-top").removeClass("fadeInDown");
 		$(".nav-top").removeClass("animated");
     }
 });
